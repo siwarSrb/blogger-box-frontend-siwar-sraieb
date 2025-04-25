66abdf7958a51db5876acbb6b550cd753e2ceb80
siwarSrb <siwarsrb@gmail.com>
2025-04-25 22:25:04 +0200

    add post and category services

--- src/app/data/category.ts
+++ src/app/data/category.ts
@@ -3,6 +3,11 @@ export interface Category {
   name: string;
 }
 
+export type CategoryCreateInput = Omit<Category, 'id'>;
+export interface CategoryUpdateInput {
+  name: string;
+}
+
 export const CATEGORIES = [
   {
     id: 'cf7df5b7-f42d-424a-bec3-1547f068b4ca',
